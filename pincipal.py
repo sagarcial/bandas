1ab03771c7a51ef99b00def60358125f2d4a6e95
Santiago Alexander Garcia Liscano <83739733+sagarcial@users.noreply.github.com>
2023-09-08 06:54:50 -0500

    Update pincipal.py

--- pincipal.py
+++ pincipal.py
@@ -1,23 +1,30 @@
-from banda import *
-from instrumento import *
+import tkinter as tk
 
-if __name__ == "__main__":
-    # Crear una banda con algunos instrumentos
-    b = Banda("Los Rítmicos", [Piano(), Guitarra(), Saxofon(), Bajo(), Flauta()])
-    
-    # Crear músicos aleatorios
-    b.crear()
-    
-    # Consultar la información inicial de la banda
+# ... Tu código para crear la banda y los músicos ...
+
+# Crear una ventana principal de Tkinter
+ventana = tk.Tk()
+ventana.title("Banda Los Rítmicos")
+
+# Función para afinar los instrumentos
+def afinar_instrumentos():
+    b.afinar_instrumentos()
     b.consultar()
-    
-    # Dar la opción de afinar los instrumentos
-    opcion = input("¿Deseas afinar los instrumentos de la banda? (S/N): ").strip().lower()
-    b.consultar()
-    print ("\nTocando sonidos de la banda:\n")
-    if opcion == 's':
-        b.afinar_instrumentos()  # Llama al método para afinar los instrumentos
-    for Musico in b.integrantes:
-        Musico.tocar()    # Consultar la información actualizada de la banda
-    
-    
+
+# Botón para afinar los instrumentos
+boton_afinar = tk.Button(ventana, text="Afinar Instrumentos", command=afinar_instrumentos)
+boton_afinar.pack()
+
+# Cargar imágenes de los instrumentos
+imagenes_instrumentos = []
+for musico in b.integrantes:
+    nombre_imagen = f"{musico.nombre}.png"  # Asegúrate de que los nombres de las imágenes coincidan con los nombres de los músicos
+    imagen = tk.PhotoImage(file=nombre_imagen)
+    imagenes_instrumentos.append(imagen)
+
+# Crear etiquetas para mostrar las imágenes
+for i, imagen in enumerate(imagenes_instrumentos):
+    etiqueta_imagen = tk.Label(ventana, image=imagen)
+    etiqueta_imagen.pack()
+
+ventana.mainloop()
